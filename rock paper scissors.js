ed38ececf1d93dc39f22868af5c50de735ce31d6
DrPeaTea <110958309+DrPeaTea@users.noreply.github.com>
2022-10-05 21:37:29 +1000

    rock paper scissors fix + new tic tac toe

--- rock paper scissors.js
+++ rock paper scissors.js
@@ -2,7 +2,6 @@ const getComputerChoice = Math.floor(Math.random() * 3);
 const rockPlayer = document.querySelector(".rock");
 const paperPlayer = document.querySelector(".paper");
 const scissorsPlayer = document.querySelector(".scissors");
-
 const optionsMap = {
   0: "rock",
   1: "paper",
@@ -14,46 +13,53 @@ rockPlayer.addEventListener("click", () => {
   let computerChoiceWord = optionsMap[computerChoice];
   gameResult("rock", computerChoiceWord);
 });
-
-function gameResult(playerChoice, computerChoiceWord) {
-  if (computerChoiceWord === "rock") {
-    alert("computer picked rock, draw");
-  } else if (computerChoiceWord === "paper") {
-    alert("computer picked paper, computer wins");
-  } else if (computerChoiceWord === "scissors") {
-    alert("computer picked scissors, you win");
-  }
-}
 paperPlayer.addEventListener("click", () => {
   let computerChoice = Math.floor(Math.random() * 3);
   let computerChoiceWord = optionsMap[computerChoice];
-  gameResult1("paper", computerChoiceWord);
+  gameResult("paper", computerChoiceWord);
 });
-
-function gameResult1(playerChoice, computerChoiceWord) {
-  if (computerChoiceWord === "rock") {
-    alert("computer picked rock, you win");
-  } else if (computerChoiceWord === "paper") {
-    alert("computer picked paper, draw");
-  } else if (computerChoiceWord === "scissors") {
-    alert("computer picked scissors, computer wins");
-  }
-}
 scissorsPlayer.addEventListener("click", () => {
   let computerChoice = Math.floor(Math.random() * 3);
   let computerChoiceWord = optionsMap[computerChoice];
-  gameResult2("scissors", computerChoiceWord);
+  gameResult("scissors", computerChoiceWord);
 });
 
-function gameResult2(playerChoice, computerChoiceWord) {
-  if (computerChoiceWord === "rock") {
+function gameResult(playerChoice, computerChoiceWord) {
+  if (computerChoiceWord === playerChoice) {
+    alert("you picked the same as computer, draw");
+  } else if (playerChoice === "rock" && computerChoiceWord === "paper") {
+    alert("computer picked paper, computer wins");
+  } else if (playerChoice === "rock" && computerChoiceWord === "scissors") {
+    alert("computer picked scissors, you win");
+  } else if (playerChoice === "paper" && computerChoiceWord === "rock") {
+    alert("computer picked rock, you win");
+  } else if (playerChoice === "paper" && computerChoiceWord === "scissors") {
+    alert("computer picked scissors, computer wins");
+  } else if (playerChoice === "scissors" && computerChoiceWord === "rock") {
     alert("computer picked rock, computer wins");
-  } else if (computerChoiceWord === "paper") {
+  } else if (playerChoice === "scissors" && computerChoiceWord === "paper") {
     alert("computer picked paper, you win");
-  } else if (computerChoiceWord === "scissors") {
-    alert("computer picked scissors, draw");
   }
 }
+// function gameResult1(playerChoice, computerChoiceWord) {
+//   if (computerChoiceWord === "rock") {
+//     alert("computer picked rock, you win");
+//   } else if (computerChoiceWord === "paper") {
+//     alert("computer picked paper, draw");
+//   } else if (computerChoiceWord === "scissors") {
+//     alert("computer picked scissors, computer wins");
+//   }
+// }
+
+// function gameResult2(playerChoice, computerChoiceWord) {
+//   if (computerChoiceWord === "rock") {
+//     alert("computer picked rock, computer wins");
+//   } else if (computerChoiceWord === "paper") {
+//     alert("computer picked paper, you win");
+//   } else if (computerChoiceWord === "scissors") {
+//     alert("computer picked scissors, draw");
+//   }
+// }
 
 // rockPlayer.addEventListener("click", () => {
 //   if (getComputerChoice === 1) {
@@ -91,4 +97,3 @@ function gameResult2(playerChoice, computerChoiceWord) {
 //     window.location.reload();
 //   }
 // });
-console.log(getComputerChoice);
--- tic tac toe.js
+++ tic tac toe.js
@@ -10,19 +10,167 @@ let gamearr = [
   ["", "", ""],
 ];
 
+const gameboard = ["", "", "", "", "", "", "", "", ""];
+
 //function
+
+function makeBotMove() {
+  let nextMove;
+
+  // Check if there are any positions that make us win
+  for (let i = 0; i < gameboard.length; i++) {
+    if (gameboard[i] !== "") {
+      continue;
+    }
+
+    const newGameboard = [...gameboard]; // Destructuring makes a new array instead of a linkQ
+    newGameboard[i] = "O";
+
+    if (checkBoard(newGameboard) === true) {
+      nextMove = i;
+      break;
+    }
+  }
+
+  if (!nextMove) {
+    // Check if there are any moves that make our opponenent win
+    for (let i = 0; i < gameboard.length; i++) {
+      if (gameboard[i] !== "") {
+        continue;
+      }
+
+      const newGameboard = [...gameboard]; // Destructuring makes a new array instead of a linkQ
+      newGameboard[i] = "X";
+
+      if (checkBoard(newGameboard) === true) {
+        nextMove = i;
+        break;
+      }
+    }
+  }
+
+  if (!nextMove) {
+    // Take first empty position
+    nextMove = gameboard.findIndex((position) => position === "");
+  }
+
+  gameboard[nextMove] = "O";
+  gameBoard.querySelector(`#position-${nextMove}`).textContent = "O";
+}
+
+// Return true if someone won, otherwise false
+function checkBoard(board) {
+  const wins = [
+    // Horizontal
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    // Vertical
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    // Diagonal
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  let isWinner = false;
+
+  wins.forEach((win) => {
+    // [6, 7, 8]
+    // win[0] = 6
+    // win[1] = 7
+    // win[2] = 8
+
+    if (
+      board[win[0]] !== "" &&
+      board[win[0]] === board[win[1]] &&
+      board[win[1]] === board[win[2]]
+    ) {
+      isWinner = true;
+    }
+  });
+
+  return isWinner;
+}
+
+let moves = 0;
 
 box.forEach((box) => {
   box.addEventListener("click", () => {
-    if (player1Turn === true && box.textContent === "") {
-      box.textContent = player1[1];
-      player1Turn = false;
-      // gamearr[box.id].push("1");
-      console.log(gamearr);
-    } else if (player1Turn === false && box.textContent === "") {
-      box.textContent = player2[1];
-      player1Turn = true;
-    } else {
+    if (box.textContent !== "") {
+      return;
+    }
+
+    box.textContent = "X";
+    gameboard[box.dataset.positionId] = "X";
+    moves = moves + 1;
+
+    if (moves > 8) {
+      alert("Its a draw!");
+      window.location.reload();
+      return;
+    }
+
+    if (checkBoard(gameboard) === true) {
+      alert("Player Won!");
+      window.location.reload();
+      return;
+    }
+
+    // Computer can take a turn
+    makeBotMove();
+    moves = moves + 1;
+
+    if (checkBoard(gameboard) === true) {
+      alert("Computer Won!");
+      window.location.reload();
     }
   });
 });
+
+// if (player1Turn === true && box.textContent === "") {
+//   box.textContent = player1[1];
+//   player1Turn = false;
+//   //gamearr[box.id].push("1");
+//   gameboard[box.id] = "X";
+//   console.log(gamearr);
+// } else if (player1Turn === false && box.textContent === "") {
+//   box.textContent = player2[1];
+//   player1Turn = true;
+
+//   gameboard[box.id] = "O";
+// }
+
+// winning conditions
+
+// if (
+//   (gameboard[0] !== "" &&
+//     gameboard[0] === gameboard[1] &&
+//     gameboard[1] === gameboard[2]) ||
+//   (gameboard[3] !== "" &&
+//     gameboard[3] === gameboard[4] &&
+//     gameboard[4] === gameboard[5]) ||
+//   (gameboard[6] !== "" &&
+//     gameboard[6] === gameboard[7] &&
+//     gameboard[7] === gameboard[8]) ||
+//   (gameboard[0] !== "" &&
+//     gameboard[0] === gameboard[3] &&
+//     gameboard[3] === gameboard[6]) ||
+//   (gameboard[1] !== "" &&
+//     gameboard[1] === gameboard[4] &&
+//     gameboard[4] === gameboard[7]) ||
+//   (gameboard[2] !== "" &&
+//     gameboard[2] === gameboard[5] &&
+//     gameboard[5] === gameboard[8]) ||
+//   (gameboard[0] !== "" &&
+//     gameboard[0] === gameboard[4] &&
+//     gameboard[4] === gameboard[8]) ||
+//   (gameboard[2] !== "" &&
+//     gameboard[2] === gameboard[4] &&
+//     gameboard[4] === gameboard[6])
+// ) {
+
+//     // }
+//   });
+// });
